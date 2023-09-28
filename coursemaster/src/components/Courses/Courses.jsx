b90d30c3dbc3783f8f13d4b49bcf52341c92981f
AnuragPatil <patilanurag661@gmail.com>
2023-09-28 17:19:30 +0530

    Courses-Footer

--- coursemaster/src/components/Courses/Courses.jsx
+++ coursemaster/src/components/Courses/Courses.jsx
@@ -3,15 +3,87 @@ import {
   Container,
   HStack,
   Heading,
+  Image,
   Input,
+  Link,
   Stack,
   Text,
+  VStack,
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import '../Home/home.css';
+
+
+const Course = ({
+  views,
+  title,
+  imageSrc,
+  id,
+  addToPlaylistHandler,
+  creator,
+  description,
+  lectureCount,
+}) => {
+  return (
+    <VStack className="course" alignItems={['center', 'flex-start']}>
+      <Image src={imageSrc} boxSize={'60'} objectFit={'contain'} />
+      <Heading
+        textAlign={['center', 'left']}
+        maxW={'200px'}
+        fontFamily={'sans-serif'}
+        noOfLines={3}
+        children={title}
+        size={'sm'}
+      />
+      <Text noOfLines={2} children={description} />
+      <HStack>
+        <Text
+          fontWeight={'bold'}
+          textTransform={'uppercase'}
+          children={'Creator'}
+        />
+        <Text
+          fontWeight={'body'}
+          textTransform={'uppercase'}
+          children={creator}
+        />
+      </HStack>
+      <Heading
+        textAlign={'center'}
+        size="xs"
+        children={`Lectures - ${lectureCount}`}
+        textTransform={'uppercase'}
+      />
+      <Heading
+        size="xs"
+        children={`Views - ${views}`}
+        textTransform={'uppercase'}
+      />
+
+      <Stack direction={['column', 'row']} alignItems={'center'}>
+        <Link to={`/course/${id}`}>
+          <Button colorScheme="blue">Watch Now</Button>
+        </Link>
+        <Button
+          variant="ghost"
+          colorScheme="blue"
+          onClick={() => addToPlaylistHandler(id)}
+        >
+          Add to playlist
+        </Button>
+      </Stack>
+    </VStack>
+  );
+};
 
 const Courses = () => {
   const [keyword, setKeyword] = useState('');
   const [category, setCategory] = useState('');
+
+  const addToPlaylistHandler = () => {
+    console.log("Added To playlist");
+  };
+
   const categories = [
     'Web development',
     'Artificial Intelligence',
@@ -20,6 +92,7 @@ const Courses = () => {
     'Data Science',
     'Game Development',
   ];
+
   return (
     <Container minH={'95vh'} maxW={'container.lg'} paddingY={'8'}>
       <Heading children="All Courses" m={'8'} />
@@ -33,7 +106,7 @@ const Courses = () => {
       <HStack
         overflowX={'auto'}
         paddingY={'8'}
-        css={{ "&::-webkit-scrollbar": { display: "none" } }}
+        css={{ '&::-webkit-scrollbar': { display: 'none' } }}
       >
         {categories.map((item, index) => (
           <Button key={index} onClick={() => setCategory(item)} minW={'60'}>
@@ -43,14 +116,21 @@ const Courses = () => {
       </HStack>
 
       <Stack
-        direction={["column","row"]}
-        flexWrap={"wrap"}
-        justifyContent={["flex-start", "space-evenly"]} 
-        alignItems={["center","flex-start"]}
+        direction={['column', 'row']}
+        flexWrap={'wrap'}
+        justifyContent={['flex-start', 'space-evenly']}
+        alignItems={['center', 'flex-start']}
       >
-
         <Course
-
+          title={'Sample'}
+          description={'Sample'}
+          views={23}
+          imageSrc={'https://www.classcentral.com/report/wp-content/uploads/2022/10/React-JS-BCG-Banner.png'}
+          id={'sample'}
+          creator={'Sample Boy'}
+          lectureCount={2}
+          addToPlaylistHandler = {addToPlaylistHandler}
+        />
       </Stack>
     </Container>
   );
