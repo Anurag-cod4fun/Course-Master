b13fddf2ff4e4a0664d40f3ceff20e3f5f1f0e91
Anurag Patil <patilanurag661@gmail.com>
2024-07-07 01:39:08 +0530

    login-logout-api

--- coursemasterserver/routes/userRoutes.js
+++ coursemasterserver/routes/userRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { register } from "../controllers/userController.js";
+import { login, register, logout } from "../controllers/userController.js";
 
 const router = express.Router();
 
 
 // To Register a new user
 router.route('/register').post(register);
+
+// To Login a user
+router.route('/login').post(login);
+
+// To Login a user
+router.route('/logout').post(logout);
 
 export default router;
